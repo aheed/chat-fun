eb2235638abb51e415e2b6455b7ed4fcf5b96c1e
Anders Heed <aheed88888@gmail.com>
2024-03-03 14:08:49 +0100

    add get_sys_info tool

--- README.md
+++ README.md
@@ -8,7 +8,7 @@ Inspired by [YouTube Video](https://www.youtube.com/watch?v=i-oHvHejdsc).
 
 Install [Langchain](https://github.com/hwchase17/langchain) and other required packages.
 ```shell
-pip install openai
+pip install openai portkey-ai
 ```
 Modify `constants.py.default` to use your own [OpenAI API key](https://platform.openai.com/account/api-keys), and to use you own [Portkey API key](https://app.portkey.ai/) rename it to `constants.py`.
 
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import subprocess
 from openai import OpenAI
 import json
 from portkey_ai import createHeaders, PORTKEY_GATEWAY_URL
@@ -16,10 +17,61 @@ client = OpenAI(
     )
 )
 
+def execute_shell_command(cmd:str) -> str:
+    try:
+        result = subprocess.run(cmd, shell=True, capture_output=True, text=True)
+        if result.returncode == 0:
+            return result.stdout.strip()  # Return the output of the command
+        else:
+            return f"Error executing command: {command}"
+    except Exception as e:
+        return f"An error occurred: {e}"
+
+def get_sys_info() -> str:
+    print("get_sys_info")
+    info = {
+        "uname_output": "Linux agrippa 6.5.0-21-generic #21~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb  9 13:32:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux",
+        "os": "Linux",
+        "arch": "x86_64",
+        "cores": "8",
+        "df_output": """
+Filesystem      Size  Used Avail Use% Mounted on
+tmpfs           3,2G  2,4M  3,2G   1% /run
+/dev/nvme0n1p5  457G  152G  283G  35% /
+tmpfs            16G  191M   16G   2% /dev/shm
+tmpfs           5,0M  4,0K  5,0M   1% /run/lock
+efivarfs        256K   46K  206K  19% /sys/firmware/efi/efivars
+/dev/nvme0n1p1   96M   32M   65M  33% /boot/efi
+tmpfs           3,2G  164K  3,2G   1% /run/user/1000
+""",
+        "lshw_output": """
+  *-display                 
+       description: VGA compatible controller
+       product: GP106 [GeForce GTX 1060 6GB] [10DE:1C03]
+       vendor: NVIDIA Corporation [10DE]
+       physical id: 0
+       bus info: pci@0000:02:00.0
+       version: a1
+       width: 64 bits
+       clock: 33MHz
+       capabilities: pm msi pciexpress vga_controller bus_master cap_list rom
+       configuration: driver=nvidia latency=0
+       resources: irq:146 memory:dd000000-ddffffff memory:c0000000-cfffffff memory:d0000000-d1ffffff ioport:e000(size=128) memory:c0000-dffff
+  *-graphics
+       product: EFI VGA
+       physical id: 2
+       logical name: /dev/fb0
+       capabilities: fb
+       configuration: depth=32 resolution=1024,768
+"""
+        }
+    return json.dumps(info)
+
 # Example dummy function hard coded to return the same weather
 # In production, this could be your backend API or an external API
 def get_current_weather(location, unit="fahrenheit"):
     """Get the current weather in a given location"""
+    print("get_current_weather")
     if "tokyo" in location.lower():
         return json.dumps({"location": "Tokyo", "temperature": "10", "unit": unit})
     elif "san francisco" in location.lower():
@@ -30,8 +82,19 @@ def get_current_weather(location, unit="fahrenheit"):
         return json.dumps({"location": location, "temperature": "unknown"})
 
 def run_conversation():
+
     # Step 1: send the conversation and available functions to the model
-    messages = [{"role": "user", "content": "What's the weather like in San Francisco, and Paris?"}]
+    messages = [
+      {
+        "role": "system",
+        "content": "You are a pessimistic assistant. You keep factual answers short but add remarks with sentiment."
+      },
+      #{"role": "user", "content": "What's the weather like in San Francisco, and Paris?"}
+      #{"role": "user", "content": "Should I upgrade to a newer operating system?"}
+      #{"role": "user", "content": "What is the name of my Linux distribution and version nickname?"}
+      #{"role": "user", "content": "3 CPU bound processes are already running and I want to start 7 more. Can all those processes run concurrently?"}
+      {"role": "user", "content": "Could I run a big LLM locally on my computer?"}
+      ]
     tools = [
         {
             "type": "function",
@@ -50,13 +113,21 @@ def run_conversation():
                     "required": ["location"],
                 },
             },
+        },
+        {
+            "type": "function",
+            "function": {
+                "name": "get_sys_info",
+                "description": "Get information about my computer"
+            },
         }
     ]
     response = client.chat.completions.create(
-        model="gpt-3.5-turbo-0125",
+        model="gpt-4-turbo-preview",
         messages=messages,
         tools=tools,
         tool_choice="auto",  # auto is default, but we'll be explicit
+        #tool_choice={"type": "function", "function": {"name": "get_sys_info"}}
     )
     response_message = response.choices[0].message
     tool_calls = response_message.tool_calls
@@ -66,6 +137,7 @@ def run_conversation():
         # Note: the JSON response may not always be valid; be sure to handle errors
         available_functions = {
             "get_current_weather": get_current_weather,
+            "get_sys_info": get_sys_info
         }  # only one function in this example, but you can have multiple
         messages.append(response_message)  # extend conversation with assistant's reply
         # Step 4: send the info for each function call and function response to the model
@@ -73,10 +145,13 @@ def run_conversation():
             function_name = tool_call.function.name
             function_to_call = available_functions[function_name]
             function_args = json.loads(tool_call.function.arguments)
-            function_response = function_to_call(
-                location=function_args.get("location"),
-                unit=function_args.get("unit"),
-            )
+            if function_to_call == get_current_weather:
+                function_response = function_to_call(
+                    location=function_args.get("location"),
+                    unit=function_args.get("unit"),
+                )
+            else:
+                function_response = function_to_call()
             messages.append(
                 {
                     "tool_call_id": tool_call.id,
@@ -86,8 +161,10 @@ def run_conversation():
                 }
             )  # extend conversation with function response
         second_response = client.chat.completions.create(
-            model="gpt-3.5-turbo-0125",
+            model="gpt-4-turbo-preview",
             messages=messages,
         )  # get a new response from the model where it can see the function response
         return second_response
+    else:
+        return response_message
 print(run_conversation())
